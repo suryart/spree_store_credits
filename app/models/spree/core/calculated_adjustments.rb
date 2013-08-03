7431f1093e12819e1048eb7661d961ac6c3fffbf
Surya Tripathi <raj.surya19@gmail.com>
2013-08-04 02:51:44 +0530

    if respond to originator

--- app/models/spree/core/calculated_adjustments.rb
+++ app/models/spree/core/calculated_adjustments.rb
@@ -4,7 +4,11 @@ module Spree
 
       # Calculate the amount to be used when creating an adjustment
       def compute_amount(calculable)
-        self.originator.respond_to?(:compute_amount) ? self.originator.compute_amount(calculable) : self.calculator.compute(calculable)
+        if self.respond_to?(:originator)
+          self.originator.respond_to?(:compute_amount) ? self.originator.compute_amount(calculable) : self.calculator.compute(calculable)
+        else
+          self.calculator.compute(calculable)
+        end
       end
     end
   end
